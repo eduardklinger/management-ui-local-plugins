302def2a8c16113405891d2195e613fe4cbecfe7
Eduard Klinger <110473990+eduardklinger@users.noreply.github.com>
2026-03-27 10:58:49 +0100

    fix(univie): scope sidebar styling

--- univie/implementations/sidebar/components/CustomNavMain.tsx
+++ univie/implementations/sidebar/components/CustomNavMain.tsx
@@ -25,12 +25,11 @@ export const CustomNavMain = (props: NavMainProps) => {
   return (
     <NavMain
       groupClassName={cn(props.open && "p-0")}
-      menuClassName={cn("flex flex-col gap-1", props.open && "gap-0")}
+      menuClassName={cn("univie-sidebar-nav flex flex-col gap-1", props.open && "gap-0")}
       customItemStyles={cn("h-12 flex gap-4 px-6", props.open && "rounded-none")}
       renderActiveIndicator={(isActive, open) =>
         isActive && open && <div className="absolute inset-y-0 left-0 w-2 bg-primary " />
       }
-      customActiveStyles={"data-[active=true]:bg-sidebar-primary"}
       {...props}
       items={translatedItems}
     />
